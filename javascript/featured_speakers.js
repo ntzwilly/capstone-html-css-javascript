cbf98c9cfebc162b7f6f60327f3c0919fe5a9882
ntzwilly <ntzwilly@gmail.com>
2021-06-30 19:29:34 +0200

    Correct linter errors

--- javascript/featured_speakers.js
+++ javascript/featured_speakers.js
@@ -1,4 +1,4 @@
-import * as data from './speakers_data.js'
+import * as data from './speakers_data.js';
 
 function elementGenerator(typeName, className, content, idName) {
   const element = document.createElement(typeName);
@@ -15,39 +15,35 @@ function elementGenerator(typeName, className, content, idName) {
 }
 
 function speakerGenerator(speaker) {
-  const featuredSpeakers = elementGenerator('section', 'featured-speakers', null, null)
-  const featuredSpeakerHeader = elementGenerator('h3', 'featured-speakers-header', null, null)
-  featuredSpeakers.appendChild(featuredSpeakerHeader)
-  const speakers = elementGenerator('div',null, null, null)
-  const speaker1 = elementGenerator('div', 'speaker', null, null)
-  const image = elementGenerator('img', null, null, null)
-  image.src= speaker.image 
-  image.alt="speaker-image"
-  speaker1.appendChild(image)
-  const div = elementGenerator('div', null, null, null)
-  speaker1.appendChild(div)
-  const speakerName = elementGenerator('h3', 'speaker-name', speaker.name, null)
+  const featuredSpeakers = elementGenerator('section', 'featured-speakers', null, null);
+  const featuredSpeakerHeader = elementGenerator('h3', 'featured-speakers-header', null, null);
+  featuredSpeakers.appendChild(featuredSpeakerHeader);
+  const speakers = elementGenerator('div', null, null, null);
+  const speaker1 = elementGenerator('div', 'speaker', null, null);
+  const image = elementGenerator('img', null, null, null);
+  image.src = speaker.image;
+  image.alt = 'speaker-image';
+  speaker1.appendChild(image);
+  const div = elementGenerator('div', null, null, null);
+  speaker1.appendChild(div);
+  const speakerName = elementGenerator('h3', 'speaker-name', speaker.name, null);
   div.appendChild(speakerName);
-  const line = elementGenerator('hr', 'speaker-line', null, null)
-  div.appendChild(line)
-  const speakerTitle = elementGenerator('p', 'speaker-title', speaker.title, null)
-  const speakerDescription = elementGenerator('p', 'speaker-description', speaker.comment, null)
-  div.appendChild(speakerTitle)
-  div.appendChild(speakerDescription)
+  const line = elementGenerator('hr', 'speaker-line', null, null);
+  div.appendChild(line);
+  const speakerTitle = elementGenerator('p', 'speaker-title', speaker.title, null);
+  const speakerDescription = elementGenerator('p', 'speaker-description', speaker.comment, null);
+  div.appendChild(speakerTitle);
+  div.appendChild(speakerDescription);
 
-
-  speakers.appendChild(speaker1)
-  featuredSpeakers.appendChild(speakers)
-  console.log(featuredSpeakers)
+  speakers.appendChild(speaker1);
+  featuredSpeakers.appendChild(speakers);
   return featuredSpeakers;
 }
 
 const speakers = document.getElementById('speakers');
-
 
 const speaker = data.default;
 speaker.forEach((p) => {
   const generatedSpeaker = speakerGenerator(p);
   speakers.appendChild(generatedSpeaker);
 });
-
--- javascript/speakers_data.js
+++ javascript/speakers_data.js
@@ -15,28 +15,26 @@ const speaker = [
     image: './images/photo6.jpg',
     name: 'Douglas Scott',
     title: 'Movie Director, and Producer',
-    comment: 'Impossible to describe... My travel was a beautiful experience'
+    comment: 'Impossible to describe... My travel was a beautiful experience',
   },
   {
     image: './images/photo7.jpg',
     name: 'William Wallace',
     title: 'English Teacher, IAT University',
-    comment: 'The service of all the staff was great they were so friendly.'
+    comment: 'The service of all the staff was great they were so friendly.',
   },
   {
     image: './images/photo8.jpg',
     name: 'Ryan Davis',
     title: 'Movies Actor',
-    comment: 'While looking for a hotel to stay while on my honeymoon, I came across this agency. my wife and I are super happy, we stayed'
+    comment: 'While looking for a hotel to stay while on my honeymoon, I came across this agency. my wife and I are super happy, we stayed',
   },
   {
     image: './images/photo9.jpg',
     name: 'Jeanne Darc',
     title: 'Chief Executive Officer, BAYER',
-    comment: 'We’re still on vacation but enjoying ourselves, the property and service so much that we must share how amazing this is...'
-  }
-]
+    comment: 'We’re still on vacation but enjoying ourselves, the property and service so much that we must share how amazing this is...',
+  },
+];
 
 export default speaker;
-  
-  
